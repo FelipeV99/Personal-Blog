612e2cab994a4144a252a8272e7f54889c475fe8
FelipeV99 <38900312+FelipeV99@users.noreply.github.com>
2022-07-25 22:30:57 -0500

    aesthetic signup

--- src/components/SignInModal/SignInModal.jsx
+++ src/components/SignInModal/SignInModal.jsx
@@ -5,6 +5,7 @@ import { auth } from '../../firebase-config.js'
 import './signInModal.css'
 import { async } from '@firebase/util'
 import ButtonSign from '../Buttons/ButtonSign/ButtonSign.jsx'
+import Field from '../Field/Field.jsx'
 import {AiOutlineClose} from 'react-icons/ai'
 
 
@@ -65,20 +66,25 @@ const SignIn = (props) => {
                 <div className='modal-style'>
                     {/* <button className='btn-sec' onClick={props.close}>Back</button> */}
                     <h1 className="text-align-center">Welcome to Blo</h1>
-                    <h1 className="text-align-center">Sign Up</h1>
-                    <ButtonSign />
-                    <ButtonSign />
-                    <p>or sign up with email</p>
-                    <label>Name</label>
+                    <h1 className="text-align-center margin-bottom-h1">Sign Up</h1>
+                    <ButtonSign service='Google' />
+                    <ButtonSign service='Facebook'/>
+                    <p id="or-p">or</p>
+                    <Field service="Name" />
+                    <Field service="Mail" />
+                    <Field service="Password" />
+                    <Field service="Confirm Password" />
+                    {/* <label>Name</label>
                     <input type="text" onChange={(e) => { userRef.current = e.target.value }} ref={userRef} />
                     <label>Mail</label>
                     <input type="text" onChange={(e) => { mailRef.current = e.target.value }} ref={mailRef} />
                     <label>password</label>
                     <input type="password" onChange={(e) => { passRef.current = e.target.value }} ref={passRef} />
-                    <input type="password" onChange={(e) => { passRef.current = e.target.value }} ref={passRef} />
-                    <button className='btn-pri' onClick={() => { register() }}>Sign Up</button>
-                    <p>Already have an account?</p><p>Login</p>
-                    <AiOutlineClose onClick={props.close} size={20}/>
+                    <label>Confirm Password</label>
+                    <input type="password" onChange={(e) => { passRef.current = e.target.value }} ref={passRef} /> */}
+                    <button className='btn-pri btn-signup' onClick={() => { register() }}>Sign Up</button>
+                    <p className='text-align-center'>Already have an account? <a>Login</a></p>
+                    <AiOutlineClose className="align-icon" onClick={props.close} size={20}/>
 
                     {/* <button onClick={() => { login() }}>Login</button>
                     <button onClick={() => { logout() }}>Logout</button>
