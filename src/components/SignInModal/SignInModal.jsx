097b48bc509dc5741db6a7f36baf4c8df30441b5
FelipeV99 <38900312+FelipeV99@users.noreply.github.com>
2022-07-29 18:42:26 -0500

    no Buttons

--- src/components/SignInModal/SignInModal.jsx
+++ src/components/SignInModal/SignInModal.jsx
@@ -13,14 +13,9 @@ const SignIn = (props) => {
     // const inputImageRef = useRef()
 
     const userRef = useRef()
-<<<<<<< HEAD
     const emailRef = useRef()
     const passwordRef = useRef()
     const confirmPasswordRef = useRef()
-=======
-    const mailRef = useRef()
-    const passRef = useRef()
->>>>>>> parent of cea794b (auth)
 
     // const placeImage = (e) => {
     //     e.preventDefault();
@@ -40,17 +35,10 @@ const SignIn = (props) => {
 
     const register = async () => {
         try {
-<<<<<<< HEAD
             const newUser = await createUserWithEmailAndPassword(auth, userRef.current, passwordRef.current)
             console.log(newUser)
         } catch {
             console.log("error")
-=======
-            const newUser = await createUserWithEmailAndPassword(auth, userRef.current, passRef.current)
-            console.log(newUser)
-        } catch {
-            console.log("erorr")
->>>>>>> parent of cea794b (auth)
         }
     }
 
@@ -61,7 +49,7 @@ const SignIn = (props) => {
 
     const login = async () => {
         try {
-            const newUser = await signInWithEmailAndPassword(auth, userRef.current, passRef.current)
+            const newUser = await signInWithEmailAndPassword(auth, userRef.current, passwordRef.current)
             console.log("lemme get in this bitch",newUser)
             
         } catch {
@@ -78,41 +66,20 @@ const SignIn = (props) => {
                 <div className='modal-style'>
                     {/* <button className='btn-sec' onClick={props.close}>Back</button> */}
                     <h1 className="text-align-center">Welcome to Blo</h1>
-<<<<<<< HEAD
                     <h1 className="text-align-center">Sign Up</h1>
                     <ButtonSign />
                     <ButtonSign />
                     <p>or sign up with email</p>
                     <label>Name</label>
-=======
-                    <h1 className="text-align-center margin-bottom-h1">Sign Up</h1>
-                    <ButtonSign service='Google' />
-                    <ButtonSign service='Facebook'/>
-                    <p id="or-p">or</p>
-                    <Field service="Name" />
-                    <Field service="Mail" />
-                    <Field service="Password" />
-                    <Field service="Confirm Password" />
-                    {/* <label>Name</label>
->>>>>>> parent of cea794b (auth)
                     <input type="text" onChange={(e) => { userRef.current = e.target.value }} ref={userRef} />
                     <label>Mail</label>
                     <input type="text" onChange={(e) => { emailRef.current = e.target.value }} ref={emailRef} />
                     <label>password</label>
-<<<<<<< HEAD
                     <input type="password" onChange={(e) => { passwordRef.current = e.target.value }} ref={passwordRef} />
                     <input type="password" onChange={(e) => { passwordRef.current = e.target.value }} ref={passwordRef} />
                     <button className='btn-pri' onClick={() => { register() }}>Sign Up</button>
                     <p>Already have an account?</p><p>Login</p>
                     <AiOutlineClose onClick={props.close} size={20}/>
-=======
-                    <input type="password" onChange={(e) => { passRef.current = e.target.value }} ref={passRef} />
-                    <label>Confirm Password</label>
-                    <input type="password" onChange={(e) => { passRef.current = e.target.value }} ref={passRef} /> */}
-                    <button className='btn-pri btn-signup' onClick={() => { register() }}>Sign Up</button>
-                    <p className='text-align-center'>Already have an account? <a>Login</a></p>
-                    <AiOutlineClose className="align-icon" onClick={props.close} size={20}/>
->>>>>>> parent of cea794b (auth)
 
                     {/* <button onClick={() => { login() }}>Login</button>
                     <button onClick={() => { logout() }}>Logout</button>
